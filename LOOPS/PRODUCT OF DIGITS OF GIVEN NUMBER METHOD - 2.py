8d61023c56d494fb906fd543bb071fed497be895
guru404e <121336270+guru404e@users.noreply.github.com>
2023-03-08 13:28:02 +0000

    Update PRODUCT OF DIGITS OF GIVEN NUMBER METHOD - 2.py

--- LOOPS/PRODUCT OF DIGITS OF GIVEN NUMBER METHOD - 2.py
+++ LOOPS/PRODUCT OF DIGITS OF GIVEN NUMBER METHOD - 2.py
@@ -1,13 +1,17 @@
 # codesbyguru
 
-num = int(input("Enter Number : "))
+def product_of_digits(num):
 
-p = 1
-
-for i in range(len(str(num))):
-    r = num % 10
-    p = p * r
-    num = num//10
+    p = 1
     
-print("Product =",p)
+    while(num):
+        r = num % 10
+        p = p * r
+        num = num//10
+        
+    return p
+    
+num = int(input("Enter Number : "))
+print("Product =",product_of_digits(num))
+    
     
