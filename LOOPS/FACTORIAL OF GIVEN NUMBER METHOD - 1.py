6eb557cc29dfbaeb13b32d54407c39beb2f51cbe
guru404e <121336270+guru404e@users.noreply.github.com>
2023-03-08 13:21:00 +0000

    Create FACTORIAL OF GIVEN NUMBER METHOD - 1.py

---
+++ LOOPS/FACTORIAL OF GIVEN NUMBER METHOD - 1.py
@@ -0,0 +1,14 @@
+# codesbyguru
+
+def factorial(num):
+    
+    fact = 1
+    
+    for i in range(1,num+1):
+        fact = fact * i
+    
+    return fact
+    
+num = int(input("Enter Number : "))
+
+print(factorial(num))
