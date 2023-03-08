3cc36c4356e55de04e8175105e22159d39a06b37
guru404e <121336270+guru404e@users.noreply.github.com>
2023-03-08 06:40:27 +0000

    Update PALINDROME OR NOT METHOD - 2.py

--- STRINGS/PALINDROME OR NOT METHOD - 2.py
+++ STRINGS/PALINDROME OR NOT METHOD - 2.py
@@ -1,14 +1,18 @@
+def ispalindrome(string):
+    
+    lst = []
+
+    lst.extend(string)
+    
+    lst2 = lst.copy()
+    
+    lst.reverse()
+    
+    if lst2 == lst:
+        return True
+    else:
+        return False
+
 string = input("Enter String  : ")
 
-lst = []
-
-lst.extend(string)
-
-lst2 = lst.copy()
-
-lst.reverse()
-
-if lst2 == lst:
-    print("Palindrome")
-else:
-    print("Not Palindrome")
+print(ispalindrome(string))
